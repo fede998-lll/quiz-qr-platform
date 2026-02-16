bd20197f02648068301a7630bce5e578feccbb88
Federico <federico.cocuzza98@gmail.com>
2026-02-16 18:27:43 +0100

    Update production base URL for live domain

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 ﻿window.APP_CONFIG = {
   SUPABASE_URL: "https://qwytztwtropbvxeuwlrp.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_zXYWbq5-cwgsF_WSpvT98A_aievt00M",
-  PUBLIC_BASE_URL: "http://192.168.1.105:8089"
+  PUBLIC_BASE_URL: "https://liveqrquiz.org"
 };
 
 
